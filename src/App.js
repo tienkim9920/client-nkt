b324759185fd95e40bf473f38f68c3bab6355718
tienkim9920 <tienkim9920@gmail.com>
2021-06-08 14:22:36 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,112 +1,23 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-import Header from "./Component/Header";
-import Home from "./Component/Home";
-import { useState } from "react";
-import One from "./Component/One";
-import Two from "./Component/Two";
-import Three from "./Component/Three";
-import Four from "./Component/Four";
-import Five from "./Component/Five";
-import Six from "./Component/Six";
-import Seven from "./Component/Seven";
-import Eight from "./Component/Eight";
-import Nine from "./Component/Nine";
-import Ten from "./Component/Ten";
-import Finish from "./Component/Finish";
-import Rank from "./Component/Rank";
-import Result from "./Component/Result";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [result, set_result] = useState({
-    name: "",
-    answer: [],
-    time: [],
-  });
-
-  const addName = (name) => {
-    set_result({ ...result, name });
-  };
-
-  const addAnswer = (value) => {
-    const answer = result.answer;
-
-    answer.push(value);
-
-    set_result({ ...result, answer });
-  };
-
-  const addTime = (value) => {
-    const time = result.time;
-
-    time.push(value);
-
-    set_result({ ...result, time });
-
-    console.log(result);
-  };
-
-  const reset_result = (value) => {
-
-    set_result({
-      name: "",
-      answer: [],
-      time: [],
-    });
-
-  };
-
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <AnimatePresence exitBeforeEnter>
-          <Switch>
-            <Route exact path="/answer">
-              <Home addName={addName} />
-            </Route>
-            <Route path="/one">
-              <One addAnswer={addAnswer} addTime={addTime} />
-            </Route>
-            <Route path="/two">
-              <Two addAnswer={addAnswer} addTime={addTime} />
-            </Route>
-            <Route path="/three">
-              <Three addAnswer={addAnswer} addTime={addTime} />
-            </Route>
-            <Route path="/four">
-              <Four addAnswer={addAnswer} addTime={addTime} />
-            </Route>
-            <Route path="/five">
-              <Five addAnswer={addAnswer} addTime={addTime} />
-            </Route>
-            <Route path="/six">
-              <Six addAnswer={addAnswer} addTime={addTime} />
-            </Route>
-            <Route path="/seven">
-              <Seven addAnswer={addAnswer} addTime={addTime} />
-            </Route>
-            <Route path="/eight">
-              <Eight addAnswer={addAnswer} addTime={addTime} />
-            </Route>
-            <Route path="/nine">
-              <Nine addAnswer={addAnswer} addTime={addTime} />
-            </Route>
-            <Route path="/ten">
-              <Ten addAnswer={addAnswer} addTime={addTime} result={result} reset_result={reset_result} />
-            </Route>
-            <Route path="/finish">
-              <Finish result={result} />
-            </Route>
-            <Route path="/rank">
-              <Rank />
-            </Route>
-            <Route path="/result">
-              <Result />
-            </Route>
-          </Switch>
-        </AnimatePresence>
-      </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
