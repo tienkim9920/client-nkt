f7d6fc4f765a1e1d3b1efe2d578e23076b91ec47
tienkim9920 <tienkim9920@gmail.com>
2021-06-10 11:37:57 +0700

    Update

--- src/Order/OrderSuccess.jsx
+++ src/Order/OrderSuccess.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 OrderSuccess.propTypes = {
@@ -6,10 +6,19 @@ OrderSuccess.propTypes = {
 };
 
 function OrderSuccess(props) {
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            window.location.href = '/history'
+        }, 2500)
+
+    }, [])
+
     return (
         <div className="container fix_order">
             <h1>You Have Ordered Successfully</h1>
-            <span style={{ fontSize: '1.2rem' }}>Please Checking Email!</span>
+            <span style={{ fontSize: '1.2rem' }}>Please Wait A Few Minutes!</span>
         </div>
     );
 }
