cde76b80612c0c2d47cc567a0647128dc9b3cc20
tienkim9920 <tienkim9920@gmail.com>
2021-06-10 14:29:17 +0700

    Update SignUp

--- src/Auth/SignUp.jsx
+++ src/Auth/SignUp.jsx
@@ -4,139 +4,48 @@ import { Link } from 'react-router-dom';
 import User from '../API/User';
 import { useForm } from "react-hook-form";
 
-SignUp.propTypes = {
-
+const defaultValues = {
+    email: "",
+    fullname: "",
+    password: ""
 };
 
 function SignUp(props) {
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-
-    const [fullname, set_fullname] = useState('')
-    const [username, set_username] = useState('')
-    const [password, set_password] = useState('')
-    const [confirm, set_confirm] = useState('')
-    const [email, set_email] = useState('')
+    const { register, handleSubmit, formState: { errors }, reset } = useForm({ defaultValues });
 
     const [show_success, set_show_success] = useState(false)
 
-    const [errorFullname, setFullnameError] = useState(false)
-    const [errorUsername, setUsernameError] = useState(false)
-    const [errorPassword, setPasswordError] = useState(false)
-    const [errorConfirm, setConfirmError] = useState(false)
-    const [errorCheckPass, setCheckPass] = useState(false)
-
     const [username_exist, set_username_exist] = useState(false)
 
-    const handler_signup = (e) => {
+    const onSubmit = async (data) => {
 
-        e.preventDefault()
-
-        if (!fullname) {
-            setFullnameError(true)
-            setUsernameError(false)
-            setPasswordError(false)
-            setConfirmError(false)
-            return
-        } else {
-            setFullnameError(false)
-            setUsernameError(false)
-            setPasswordError(false)
-            setConfirmError(false)
-
-            if (!username){
-                setFullnameError(false)
-                setUsernameError(true)
-                setPasswordError(false)
-                setConfirmError(false)
-                return
-            }else{
-                setFullnameError(false)
-                setUsernameError(false)
-                setPasswordError(false)
-                setConfirmError(false)
-
-                if (!password){
-                    setFullnameError(false)
-                    setUsernameError(false)
-                    setPasswordError(true)
-                    setConfirmError(false)
-                    return
-                }else{
-                    setFullnameError(false)
-                    setUsernameError(false)
-                    setPasswordError(false)
-                    setConfirmError(false)
-
-                    if (!confirm){
-                        setFullnameError(false)
-                        setUsernameError(false)
-                        setPasswordError(false)
-                        setConfirmError(true)
-                        return
-                    }else{
-                        setFullnameError(false)
-                        setUsernameError(false)
-                        setPasswordError(false)
-                        setConfirmError(false)
-
-                        if (password !== confirm){
-                            setFullnameError(false)
-                            setUsernameError(false)
-                            setPasswordError(false)
-                            setConfirmError(false)
-                            setCheckPass(true)
-                            return
-                        }else{
-                            setConfirmError(false)
-                            setCheckPass(false)
-                            
-                            const fetchData = async () => {
-                                
-                                const data = {
-                                    id_user: Math.random().toString(),
-                                    username: username,
-                                    password: password,
-                                    fullname: fullname,
-                                    email: email,
-                                    id_permission: '6087dcb5f269113b3460fce4'
-                                }
-
-                                const response = await User.Post_User(data)
-
-                                console.log(response)
-
-                                if (response === 'User Da Ton Tai'){
-                                    set_username_exist(true)
-                                }else{
-                                    set_show_success(true)
-
-                                }  
-                            }
-
-                            fetchData()
-
-                            set_fullname('')
-                            set_username('')
-                            set_password('')
-                            set_fullname('')
-                            set_confirm('')
-
-                        }
-
-                    }
-                    
-                }
+            const body = {
+                username: data.username,
+                password: data.password,
+                fullname: data.fullname,
+                email: data.email,
+                id_permission: '6087dcb5f269113b3460fce4'
             }
 
-        }
-        
-        setTimeout(() => {
-            set_show_success(false)
-        }, 1500)
+            const response = await User.Post_User(body)
 
-    }
+            console.log(response)
+
+            if (response === 'User Da Ton Tai'){
+                set_username_exist(true)
+            }else{
+                set_show_success(true)
+                set_username_exist(false)
+                
+                reset({ defaultValues })
+            }
+
+            setTimeout(() => {
+                set_show_success(false)
+            }, 2500)
+
+    } 
 
 
     return (
@@ -168,47 +77,32 @@ function SignUp(props) {
                 <div className="container">
                     <div className="row">
                         <div className="col-sm-12 col-md-12 col-lg-6 col-xs-12 mr_signin">
-                            <form action="#">
+                            <form onSubmit={handleSubmit(onSubmit)}>
                                 <div className="login-form">
                                     <h4 className="login-title">Register</h4>
                                     <div className="row">
                                         <div className="col-md-12 mb-20">
                                             <label>Email *</label>
-                                            <input className="mb-0" type="text" placeholder="First Name" value={email} onChange={(e) => set_email(e.target.value)} />
+                                            <input placeholder="Enter Email" className="mb-0" id="email" {...register('email', { required: true })} />
+                                                {errors.email && errors.email.type === "required" && <span style={{ color: 'red' }}>* Email is required</span>}
                                         </div>
                                         <div className="col-md-12 mb-20">
                                             <label>Full Name *</label>
-                                            <input className="mb-0" type="text" placeholder="First Name" value={fullname} onChange={(e) => set_fullname(e.target.value)} />
-                                            {
-                                                errorFullname && <span style={{ color: 'red' }}>* Fullname is required!</span>
-                                            }  
+                                            <input placeholder="Enter Fullname" className="mb-0" id="fullname" {...register('fullname', { required: true })} />
+                                                {errors.fullname && errors.fullname.type === "required" && <span style={{ color: 'red' }}>* Fullname is required</span>}
                                         </div>
                                         <div className="col-md-12 mb-20">
                                             <label>Username *</label>
-                                            <input className="mb-0" type="text" placeholder="Username" value={username} onChange={(e) => set_username(e.target.value)} />
+                                            <input placeholder="Enter Username" className="mb-0" id="username" {...register('username', { required: true })} />
+                                                {errors.username && errors.username.type === "required" && <span style={{ color: 'red' }}>* Username is required</span>}
                                             {
-                                                errorUsername && <span style={{ color: 'red' }}>* Username is required!</span>
-                                            }
-                                            {
-                                                username_exist && <span style={{ color: 'red' }}>* Username is Existed!</span>
+                                                username_exist && <span style={{ color: 'red' }}>* Username is existed</span>
                                             }
                                         </div>
-                                        <div className="col-md-6 mb-20">
+                                        <div className="col-md-12 mb-20">
                                             <label>Password *</label>
-                                            <input className="mb-0" type="password" placeholder="Password" value={password} onChange={(e) => set_password(e.target.value)} />
-                                            {
-                                                errorPassword && <span style={{ color: 'red' }}>* Password is required!</span>
-                                            }
-                                        </div>
-                                        <div className="col-md-6 mb-20">
-                                            <label>Confirm Password *</label>
-                                            <input className="mb-0" type="password" placeholder="Confirm Password" value={confirm} onChange={(e) => set_confirm(e.target.value)} />
-                                            {
-                                                errorConfirm && <span style={{ color: 'red' }}>* Confirm Password is required!</span>
-                                            }
-                                            {
-                                                errorCheckPass && <span style={{ color: 'red' }}>* Checking Again Confirm Password!</span>
-                                            }
+                                            <input placeholder="Enter Password" type="password" className="mb-0" id="password" {...register('password', { required: true })} />
+                                                {errors.password && errors.password.type === "required" && <span style={{ color: 'red' }}>* Password is required</span>}
                                         </div>
                                         <div className="col-md-12 mb-20">
                                             <div className="d-flex justify-content-end">
@@ -216,7 +110,7 @@ function SignUp(props) {
                                             </div>
                                         </div>
                                         <div className="col-12">
-                                            <button className="register-button mt-0" style={{ cursor: 'pointer' }} onClick={handler_signup}>Register</button>
+                                            <button type="submit" className="register-button mt-0" style={{ cursor: 'pointer' }}>Register</button>
                                         </div>
                                     </div>
                                 </div>
